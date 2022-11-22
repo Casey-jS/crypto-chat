781a5d421d3ec2205b3522c20def0fa1b17d8a17
Casey Sytsema <sytsemacj@gmail.com>
2022-11-22 10:50:12 -0500

    Adds starter code, class notes, threading example

---
+++ threads.c
@@ -0,0 +1,33 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+// arg/param must be void pointer
+void* handleClient(void* arg){
+    int clientSocket = *(int*)arg; // cast as int pointer and dereference to get int
+    char line[5000];
+    recvf(clientSocket, line, 5000, 0);
+    close(clientSocket);
+}
+
+int main(int argc, char** argv){
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serveraddr, clientaddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(9876);
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    listen(sockfd, 10);
+
+    while(1){
+        socklen_t len = sizeof(clientaddr);
+        int clientsocket = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
+        pthread_t child;
+        pthread_create(&child, NULL, handleClient, &clientsocket); // fourth param: what is passed into third param
+        pthread_detach(child);
+    }
+}
