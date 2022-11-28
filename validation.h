b53f101ee325d35fbba496eff1386ad328507b3e
Seth Steinbrook <steinbrs@mail.gvsu.edu>
2022-11-27 20:54:54 -0500

    Change username

--- validation.h
+++ validation.h
@@ -6,7 +6,5 @@
 
 int handle_command(char *line, char command, char user[10]);
 bool valid_username(std::string username);
-bool valid_password(std::string password);
-bool become_admin();
 
 #endif
